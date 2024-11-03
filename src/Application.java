99e872b4d9d82cdac30b25a857bd952fb24878ae
Evgeniy Haritonenko <haritonenko04@mail.ru>
2024-11-03 23:29:36 +0700

    1st commit

---
+++ src/Application.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+public class Application {
+
+    public static void main(String[] args){
+        // Логика меню
+        // 1. Шифрование
+        // 2. Расшифровка с ключом
+        // 3. Brute force
+        // 4. Статистический анализ
+        // 0. Выход
+        System.out.println("Шифровальщик метьодом цезароя");
+        System.out.println("Выберите пункт меню");
+        System.out.println("1. Шифрование с ключом");
+        System.out.println("2. Расшифровка с ключом");
+        System.out.println("3. Brute force");
+        System.out.println("0. Выход");
+
+        Scanner console = new Scanner(System.in);
+        int j = Integer.parseInt(console.nextLine());
+        System.out.println("This is happy day");
+
+
+
+    }
+}
