3198e27ce58ecd5f7781295547eb44b3d225018d
Evgeniy Haritonenko <haritonenko04@mail.ru>
2024-11-04 22:45:11 +0700

    1st commit of doing project

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,6 @@
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 public class Application {
 
@@ -9,20 +11,36 @@ public class Application {
         // 3. Brute force
         // 4. Статистический анализ
         // 0. Выход
-        System.out.println("Шифровальщик методом цезароя");
+        System.out.println("Шифровальщик методом цезаря");
         System.out.println("Выберите пункт меню");
         System.out.println("1. Шифрование с ключом");
         System.out.println("2. Расшифровка с ключом");
         System.out.println("3. Brute force");
         System.out.println("0. Выход");
-
         Scanner console = new Scanner(System.in);
-        int j = Integer.parseInt(console.nextLine());
-        System.out.println("This is happy day");
-        System.out.println("This is happy day else");
-        System.out.println("This is happy day else no");
-        System.out.println("This is happy day else no 1");
-        System.out.println("This is happy day else no 2");
+        int choice = Integer.parseInt(console.nextLine());
+        switch(choice){
+            case 1 :
+                System.out.println("Выполняется шифрование ключом");
+                System.out.println("Введитe ключ : ");
+
+            case 2 :
+                System.out.println("Выполняется расшифровка с ключом");
+                System.out.println("Введитe ключ : ");
+            case 3 :
+                System.out.println("Выполняется расшифровка текста с помощью brute force");
+
+            default:
+                System.out.println("Выход");
+
+
+
+        }
+
+
+
+
+
 
 
 
--- src/service/Cypher.java
+++ src/service/Cypher.java
@@ -6,13 +6,15 @@ import java.nio.file.Paths;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.FileReader;
+
 
 
 
 public class Cypher {
+    private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
+            'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
+            'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
+    private static final int ALPHABET_SIZE = ALPHABET.length;
 
     public static void OpenFileforEncryption(){
         System.out.println("Введите путь к файлу, который должен быть зашифрован: ");
@@ -20,9 +22,12 @@ public class Cypher {
         String Filepath = console.nextLine();
         Path path = Paths.get(Filepath);
         if(Files.exists(path)){
-            try{
-                String data = Files.readString(path);
-                System.out.println("File data: \n" + data );
+            try(BufferedReader reader = Files.newBufferedReader(path)){
+                String line;
+                System.out.println("File data:");
+                while((line=reader.readLine())!=null){
+                    System.out.println(line);
+                }
             }
             catch(IOException e){
                 System.out.println("Error of reading the file");
@@ -33,6 +38,11 @@ public class Cypher {
             System.out.println("The specified file does not exist");
         }
         console.close();
+
+    }
+    public void Encryption(String text, int key){
+
+
 
     }
 }
